adc98e3ab956e7a5de1046c76a6f60afa02e21c8
Shaily Shekhar <shailyshekhar123@gmail.com>
2024-10-26 21:12:08 +0530

    UI and frontend of gemini

---
+++ src/assets/assets.js
@@ -0,0 +1,33 @@
+import history_icon from './history-icon.png'
+import menu_icon from './menu-icon.png'
+import plus_icon from './plus-icon.png'
+import q_icon from './q-icon.png'
+import setting_icon from './setting-icon.png'
+import bulb_icon from './bulb_icon.png'
+import compass_icon from './compass_icon.png'
+import gallery_icon from './gallery-icon.png'
+import mic_icon from './mic-icon.png'
+import user_icon from './user-icon.png'
+import yt_icon from './yt-icon.png'
+import message_icon from './message-icon.png'
+import code_icon from './code_icon.png'
+import send_icon from './send-icon.png'
+import gemini_icon from './gemini-icon.png'
+
+export const assets = {
+    history_icon,
+    menu_icon,
+    plus_icon,
+    q_icon,
+    send_icon,
+    setting_icon,
+    bulb_icon,
+    code_icon,
+    compass_icon,
+    gallery_icon,
+    mic_icon,
+    user_icon,
+    yt_icon,
+    message_icon,
+    gemini_icon
+}
