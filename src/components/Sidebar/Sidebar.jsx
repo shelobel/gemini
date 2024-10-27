7cb3380c7124a9ac083d52dcf5031abafcc7682f
Shaily Shekhar <shailyshekhar123@gmail.com>
2024-10-27 13:02:09 +0530

    UI of the result and recent chat visibility

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
+
+
 const Sidebar = () => {
 
     const [extended,setExtended] = useState(true)
+    const {onSent, prevPrompts, setRecentPrompt} = useContext(Context)
+
 
   return (
     <div className='sidebar'>
@@ -17,11 +22,17 @@ const Sidebar = () => {
             ? 
             <div className='recent'>
             <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-            <img src={assets.message_icon} alt="" />
-            <p>What is react ...</p>
-            </div>
-        </div> : null
+            {prevPrompts.map((item,index )=> {
+                return (
+                    <div className="recent-entry">
+                        <img src={assets.message_icon} alt="" />
+                        <p>{item.slice(0,18)} ...</p>
+                    </div>
+                )
+            })}
+           
+        </div> 
+        : null
         }
             
         </div>
